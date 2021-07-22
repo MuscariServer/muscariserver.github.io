7b47fa3224e0b8a482164e2691f4691eeb224415
eighty88 <49605393+eighty88@users.noreply.github.com>
2021-07-22 10:50:44 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <base href="https://eighty88.github.io/MuscariWeb/"></base>
     <Component {...pageProps}/>
     <Header></Header>
     </>
