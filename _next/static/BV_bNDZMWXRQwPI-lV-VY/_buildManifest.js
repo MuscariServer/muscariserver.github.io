3c0dad0de5c00a93a0de730047e3bc05918dbb90
eighty88 <eighty88@users.noreply.github.com>
2021-07-22 05:47:45 +0000

    deploy: dfe7d4341022eb5544d4dcc301e1364b11e7aa40

---
+++ _next/static/BV_bNDZMWXRQwPI-lV-VY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/304-1b57562b8a717858d3a5.js","static/css/1743cba5b92a53d90dfa.css","static/chunks/pages/index-b52214e330079899674c.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
