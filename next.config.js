fca01d5cc5b863614958ecc5c4584a0c09b79364
eighty88 <49605393+eighty88@users.noreply.github.com>
2021-07-23 12:04:24 +0900

    一度プロジェクトを刷新

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
+  reactStrictMode: true,
   assetPrefix: process.env.GITHUB_PAGES ? '/MuscariWeb' : '',
-};
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,7 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    'box-icon': { 
-      type: string
-      name: string
-      color: string
-      size: string 
-      rotate: string 
-      flip: string 
-      border: string 
-      animation: string
-      pull: string }
-  }
-}
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <div>{typeof window === 'undefined' ? null : <Component {...pageProps} />}</div>
-    </>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
