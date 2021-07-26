df4523b34a7a06eb3eb275460e880b0af0ef9d08
eighty88 <49605393+eighty88@users.noreply.github.com>
2021-07-26 19:36:26 +0900

    Update

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { IconContext } from 'react-icons';
 
 // import Icons from 'react-icons'
@@ -16,7 +16,7 @@ const SidebarNav = styled.div<{ sidebar: boolean }>`
     position: fixed;
     top: 0;
     background: #11101D;
-    width: ${({ sidebar }) => (sidebar ? '300px' : '80px')};
+    width: ${({ sidebar }) => (sidebar ? '400px' : '80px')};
     transition: 0.5s;
 `
 
@@ -39,6 +39,59 @@ const NavIconClose = styled.div<{ sidebar: boolean }>`
 `
 
 const SidebarWrap = styled.div``;
+const SidebarMenuWrap = styled.li`
+    position: absolute;
+    top: 75px;
+    left: 23px;
+    list-style: none;
+    display: inline-block;
+`;
+
+const MenuItem = styled(Link)`
+    font-size: 2rem;
+    position: absolute;
+    display: inline-block;
+    overflow-x: auto;
+    white-space: nowrap;
+`
+
+const ToolTip = styled.span<{ sidebar: boolean }>`
+    position: absolute;
+    display: ${({ sidebar }) => (sidebar ? 'inline-block' : 'none')};
+    top: -20px;
+    left: calc(100% + 15px);
+    z-index: 3;
+    background: #fff;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    padding: 6px 12px;
+    border-radius: 4px;
+    font-size: 15px;
+    font-weight: 400;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 0.4s ease;
+    hover {
+        opacity: 1;
+        pointer-events: auto;
+        transition: all 0.4s ease;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+`
+
+const Name = styled.span<{ sidebar: boolean }>`
+    display: ${({ sidebar }) => (sidebar ? 'inline-block' : 'none')};
+    position: relative;
+    margin-left: 12.5px;
+    top: -4px;
+    font-size: 1.75rem;
+    color: gray;
+    color: #fff;
+    white-space: nowrap;
+    opacity: ${({ sidebar }) => (sidebar ? '1' : '0')};
+    pointer-events: none;
+    transition: all 0.4s;
+`
 
 const Sidebar: FC = () => {
     const [sidebar, setSidebar] = useState(false);
@@ -54,6 +107,13 @@ const Sidebar: FC = () => {
                     <NavIconClose onClick={showSidebar} sidebar={sidebar}>
                         <HiOutlineMenuAlt3 />
                     </NavIconClose>
+                    <SidebarMenuWrap>
+                        <MenuItem to="/">
+                            <AiOutlineHome />
+                            <ToolTip>トップ</ToolTip>
+                            <Name sidebar={sidebar}>トップ</Name>
+                        </MenuItem>
+                    </SidebarMenuWrap>
                 </SidebarWrap>
             </SidebarNav>
         </IconContext.Provider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Sidebar from '../components/sidebar'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { HashRouter, Route, Link, Switch } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Sidebar></Sidebar>
       <Route path="/">
       </Route>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
