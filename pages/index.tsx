85de7c6d546e94a43bfe102288b0c2a8cd3a0b8d
eight_y_88 <jadec.eight@gmail.com>
2021-07-28 15:24:10 +0900

    Background

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,18 @@ import Sidebar from '../components/sidebar'
 import { HashRouter, Route, Link, Switch } from 'react-router-dom'
 
 export default function Home() {
+  var id = Math.floor(Math.random() * 18) + 1;
   return (
     <HashRouter>
       <Sidebar></Sidebar>
-      <Route path="/">
-      </Route>
+      <div style={{ 
+        backgroundImage: `url(/images/background/` + id + `.png)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: `cover`
+      }} className={styles.container}>
+        <Route path="/">
+        </Route>
+      </div>
     </HashRouter>
   )
 }
