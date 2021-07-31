c2471276a2630f693dbb4dbaa2c61e9fbbe66e61
eight_y_88 <jadec.eight@gmail.com>
2021-07-31 12:03:02 +0900

    Donates完成

--- components/pages/donate.tsx
+++ components/pages/donate.tsx
@@ -4,6 +4,7 @@ import styled, { keyframes } from 'styled-components';
 import { IconContext } from 'react-icons';
 import { motion } from 'framer-motion';
 import { Table } from "antd";
+import Donates from './donates';
 
 const DIV = styled.div`
     width: 100vh;
@@ -17,88 +18,6 @@ const TableWrapper = styled.div<{ visible: boolean }>`
     opacity: ${({ visible }) => (visible ? 1 : 0)};
     transition: 0.2s;
 `
-
-const columns2 = [
-    {
-        title: '日付',
-        dataIndex: 'date',
-        key: 'date',
-    },
-    {
-        title: '寄付者様',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: '内容',
-        dataIndex: 'value',
-        key: 'value',
-    },
-];
-
-const data = [
-    {
-        key: '1',
-        date: '2017/12/27',
-        name: 'Kabiemon様',
-        value: '2000円(Amazonギフト券)',
-    }, {
-        key: '2',
-        date: '2019/01/11',
-        name: 'yuu_111様',
-        value: '3000円(Amazonギフト券)',
-    }, {
-        key: '3',
-        date: '2019/01/13',
-        name: 'Kabiemon様',
-        value: '4000円(Amazonギフト券)',
-    }, {
-        key: '4',
-        date: '2019/01/19',
-        name: 'Kamesuta様',
-        value: '2000円(Amazonギフト券)+Planetary Annihilation: Titans(Steam)',
-    }, {
-        key: '5',
-        date: '2019/01/21',
-        name: 'GetNullpo様',
-        value: '10000円(Amazonギフト券)',
-    }, {
-        key: '6',
-        date: '2019/01/22',
-        name: 'Kabiemon様',
-        value: '10000円(Amazonギフト券)',
-    }, {
-        key: '7',
-        date: '2019/01/23',
-        name: 'diesu様',
-        value: '10000円(Amazonギフト券)',
-    }, {
-        key: '8',
-        date: '2019/02/09',
-        name: 'Kabiemon様',
-        value: '10000円(Amazonギフト券)',
-    }, {
-        key: '9',
-        date: '2019/07/01',
-        name: 'ayumuttu様',
-        value: '2000円(Amazonギフト券)',
-    }, {
-        key: '10',
-        date: '2019/11/09',
-        name: 'kawarasin様',
-        value: '1000円(Amazonギフト券)',
-    }, {
-        key: '11',
-        date: '2019/12/18',
-        name: 'ARTLab様',
-        value: 'Intel Core i7-7700K + JOUJYE Nano Diamond Thermal Grease OC 7',
-    }, {
-        key: '12',
-        date: '2020/10/13',
-        name: 'Kass2533様',
-        value: 'MX500 500GB + WB Blue 4TB 5400RPM',
-    },
-];
 
 function Donate() {
     const [list, setList] = useState(false);
@@ -164,9 +83,7 @@ function Donate() {
                     cursor: 'pointer',
                 }}>寄付者のリスト(クリックで表示)</div>
                 <TableWrapper visible={list} align="center">
-                    <Table dataSource={data} columns={columns2} style={{
-
-                    }} />
+                    <Donates/>
                 </TableWrapper>
                 <br />
             </DIV>
