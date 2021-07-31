30cc40d942c956a76370b019a6c4319276a56d91
eight_y_88 <jadec.eight@gmail.com>
2021-07-31 18:22:43 +0900

    Update

--- components/pages/join.tsx
+++ components/pages/join.tsx
@@ -27,11 +27,15 @@ function Join() {
         history.push("/join/1");
     }
 
+    if(page === "15") {
+        history.push("/join/14");
+    }
+
     switch(pageID) {
         case 1:
             image = "images/discord/discord.png";
             message = "当サーバーでは遊ぶためにDiscord連携が必要となっております。\n通話の強要は一切ありませんのでご安心下さい。\nまた、これ以降でわからないことがあったらDiscordまたはお問い合わせフォームで質問して下さい。"
-            href = "https://discord.gg/nWsAFjJy";
+            href = "https://discord.gg/mTbgeP8";
             break;
         case 2:
             image = "images/GDL/GD0.png";
@@ -58,7 +62,34 @@ function Join() {
         case 7:
             image = "images/GDL/GD4.png";
             message = "\"Import Zip\" を選択し、ダウンロードしたzipファイルを開きます。\nそうしたら右下の「→」ボタンをクリックします。\n次のページは適当に名前を決めて「→」ボタンをクリックしましょう。"
-
+            break;
+        case 8:
+            image = "images/setup/0.png";
+            message = "出来上がったインスタンスを起動します。";
+            break;
+        case 9:
+            image = "images/setup/1.png";
+            message = "起動したら、サーバーアドレス: \"muscari.fun\"にアクセスします。";
+            break;
+        case 10:
+            image = "images/setup/2.png";
+            message = "そうするとこういう画面になるので、Discordを開きます。"
+            break;
+        case 11:
+            image = "images/setup/3.png";
+            message = "Muscariサーバーの右上にDiscordSRVというBotがいるので右クリックします。"
+            break;
+        case 12:
+            image = "images/setup/4.png";
+            message = "そうしたら、「メッセージ」をクリックしDMを開きます。"
+            break;
+        case 13:
+            image = "images/setup/5.png";
+            message = "そして、先程のMinecraftの画面に出ている数字を送信します。"
+            break;
+        case 14:
+            image = "images/setup/6.png";
+            message = "この返信が帰ってきたら、Minecraftのサーバーに入り直してみてください。\nこれで終了です。お疲れさまでした。"
 
     }
     return (
