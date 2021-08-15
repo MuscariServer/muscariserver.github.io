120bef59b23789faa1a2290407e8aa967a3a162f
eight_y_88 <jadec.eight@gmail.com>
2021-08-15 20:22:17 +0900

    Update

--- components/pages/donate.tsx
+++ components/pages/donate.tsx
@@ -4,11 +4,7 @@ import styled from 'styled-components';
 import Donates from './donates';
 
 const DIV = styled.div`
-    width: 100vh;
-    overflow-y: auto;
-    height: 100vh;
-    background-color: rgba(255,255,255,0.5);
-    position: center;
+    background-color: rgba(255, 255, 255, 0.5);
     display: inline-block;
 `
 
@@ -35,53 +31,62 @@ function Donate() {
             transition={{
                 duration: 0.2
             }}
-
-            style={{
-                textAlign: 'center',
-            }}
         >
-            <DIV>
-                <h1>寄付</h1><br />
-                <li style={{
-                    listStyle: 'none',
-                }}>
-                    Muscari Serverは月額のレンタルサーバーではなく<br />初期費用が必要な自宅サーバーを利用しています。<br />
-                    しかし電気代やパーツ交換など<br />多少の費用がかかるため寄付を募集しています。
-                </li>
-                <ul>
-                    <h3>寄付によってできること</h3>
-                    <li>サーバーの長期運営(モチベ維持・向上)</li>
-                    <li>サーバーの強化(タイムアウト発生の防止、ラグ軽減)</li>
-                </ul>
-                <ul>
-                    <h3>寄付の特典</h3>
-                    <li>Discordの役職[VIP]の付与</li>
-                    <li>Blogカテゴリに寄付者チャンネルの追加</li>
-                    <li>ゲーム内で装着可能なバッジ</li>
-                    <li>??????????</li>
-                </ul>
-                <ul>
-                    <h2>寄付の方法</h2>
-                    <li>オーナー(大佐(rkcyk706))にDiscordのDMで<br />Amazonギフト券のコードを伝える。<br />※個人宛かサーバー宛か伝えて下さい。</li>
-                    <li>大佐のAmazon欲しい物リストを買う。</li>
-                    <li>寄付内容については全内容をWikiにて公開します。</li>
-                </ul>
-                <br />
-                <a href="https://www.amazon.co.jp/registry/wishlist/2K8VZWQTFZSYU/ref=cm_sw_r_cp_ep_ws_rZzrCbT42HZXH" style={{
-                    color: 'blue',
-                    fontSize: '1.5em',
-                }}>大佐の欲しい物リスト</a>
-            </DIV>
-            <DIV>
-                <div onClick={setListVisible} style={{
-                    fontSize: '1.5em',
-                    cursor: 'pointer',
-                }}>寄付者のリスト(クリックで表示)</div>
-                <TableWrapper visible={list} align="center">
-                    <Donates />
-                </TableWrapper>
-                <br />
-            </DIV>
+            <div style={{
+                textAlign: 'center',
+                verticalAlign: 'middle',
+                display: 'inline-block',
+                backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                height: '75vh',
+                width: '50vh',
+            }}>
+                    <h1>寄付</h1><br />
+                    <li style={{
+                        listStyle: 'none',
+                    }}>
+                        Muscari Serverは月額のレンタルサーバーではなく<br />初期費用が必要な自宅サーバーを利用しています。<br />
+                        しかし電気代やパーツ交換など<br />多少の費用がかかるため寄付を募集しています。
+                    </li>
+                    <ul>
+                        <h3>寄付によってできること</h3>
+                        <li>サーバーの長期運営(モチベ維持・向上)</li>
+                        <li>サーバーの強化(タイムアウト発生の防止、ラグ軽減)</li>
+                    </ul>
+                    <ul>
+                        <h3>寄付の特典</h3>
+                        <li>Discordの役職[VIP]の付与</li>
+                        <li>Blogカテゴリに寄付者チャンネルの追加</li>
+                        <li>ゲーム内で装着可能なバッジ</li>
+                        <li>??????????</li>
+                    </ul>
+                    <ul>
+                        <h2>寄付の方法</h2>
+                        <li>オーナー(大佐(rkcyk706))にDiscordのDMで<br />Amazonギフト券のコードを伝える。<br />※個人宛かサーバー宛か伝えて下さい。</li>
+                        <li>大佐のAmazon欲しい物リストを買う。</li>
+                        <li>寄付内容については全内容をWikiにて公開します。</li>
+                    </ul>
+                    <br />
+                    <a href="https://www.amazon.co.jp/registry/wishlist/2K8VZWQTFZSYU/ref=cm_sw_r_cp_ep_ws_rZzrCbT42HZXH" style={{
+                        color: 'blue',
+                        fontSize: '1.5em',
+                    }}>大佐の欲しい物リスト</a>
+            </div>
+            <div style={{
+                textAlign: 'center',
+                verticalAlign: 'middle',
+                display: 'inline-block',
+                backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                height: '75vh',
+                width: '75vh',
+            }}>
+                    <div onClick={setListVisible} style={{
+                        fontSize: '1.5em',
+                        cursor: 'pointer',
+                    }}>寄付者のリスト(クリックで表示)</div>
+                    <TableWrapper visible={list} align="center">
+                        <Donates />
+                    </TableWrapper>
+            </div>
         </motion.div>
     )
 }
