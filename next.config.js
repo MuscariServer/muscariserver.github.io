583710179216f4e7647e73668462ed7322bad42a
eight_y_88 <jadec.eight@gmail.com>
2021-08-21 16:43:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: process.env.GITHUB_PAGES ? '/WebSite' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? '' : '',
 }
