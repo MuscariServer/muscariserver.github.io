b86346e7f189a9f5bb6579f314477a2bec17bf47
eight_y_88 <jadec.eight@gmail.com>
2021-10-04 15:46:06 +0900

    CurseForgeへの対応 一応完了

--- components/data/join.tsx
+++ components/data/join.tsx
@@ -84,4 +84,69 @@ export const CurseForge = {
         "message": "当サーバーでは遊ぶためにDiscord連携が必要となっております。\n通話の強要は一切ありませんのでご安心下さい。\nまた、これ以降でわからないことがあったらDiscordまたはお問い合わせフォームで質問して下さい。\n上の画像をクリックでDiscordに参加できます。",
         "href": "https://discord.gg/mTbgeP8"
     },
+    2: {
+        "image": "images/CF/CF0.png",
+        "message": "当サーバーではGDLauncherやCurseForgeをModの導入に採用しております。\nこのページはCurseForgeの解説です。\nMod構成ごとにMinecraftのインスタンスを作成しているため、\n他のMod環境で遊ぶ際に再導入の必要がありません。\n上の画像をクリックでダウンロードページに飛べます。",
+        "href": "https://curseforge.overwolf.com/"
+    },
+    3: {
+        "image": "images/CF/CF0.png",
+        "message": "CurseForgeをセットアップし、開きます。",
+        "href": ""
+    },
+    4: {
+        "image": "images/CF/CF1.png",
+        "message": "Minecraftを設定します。\nやり方を忘れましたが、ランチャーの場所を指定して多分自動でできます。",
+        "href": ""
+    },
+    5: {
+        "image": "images/download.jpg",
+        "message": "上の画像からModPackのzipファイルをダウンロードします。",
+        "href": Modpack_data.zip
+    },
+    6: {
+        "image": "images/CF/CF2.png",
+        "message": "右上の「+ Create Custom Profile」ボタンをクリックします。",
+        "href": ""
+    },
+    7: {
+        "image": "images/CF/CF3.png",
+        "message": "\"Import\" を選択し、ダウンロードしたzipファイルを開きます。\nそうすると自動でインストールされます。",
+        "href": ""
+    },
+    8: {
+        "image": "images/CF/CF4.png",
+        "message": "出来上がったインスタンスを起動します。",
+        "href": ""
+    },
+    9: {
+        "image": "images/setup/1.png",
+        "message": "起動したら、サーバーアドレス: \"muscari.fun\"にアクセスします。",
+        "href": ""
+    },
+    10: {
+        "image": "images/setup/2.png",
+        "message": "そうするとこういう画面になるので、Discordを開きます。",
+        "href": ""
+    },
+    11: {
+        "image": "images/setup/3.png",
+        "message": "Muscariサーバーの右上にDiscordSRVというBotがいるので右クリックします。",
+        "href": ""
+    },
+    12: {
+        "image": "images/setup/4.png",
+        "message": "そうしたら、「メッセージ」をクリックしDMを開きます。",
+        "href": ""
+    },
+    13: {
+        "image": "images/setup/5.png",
+        "message": "そして、先程のMinecraftの画面に出ている数字を送信します。",
+        "href": ""
+    },
+    14: {
+        "image": "images/setup/6.png",
+        "message": "この返信が帰ってきたら、Minecraftのサーバーに入り直してみてください。\nこれで終了です。お疲れさまでした。",
+        "href": ""
+    },
 };
