a0dcc36ff0d7c1e6c0b238da8287d2459d0bb811
eight_y_88 <jadec.eight@gmail.com>
2021-10-14 19:59:03 +0900

    スペックを設定に移動

--- components/pages/info/spec.tsx
+++ components/pages/info/spec.tsx
@@ -1,6 +1,7 @@
 import { Table } from "antd";
 import { motion } from "framer-motion";
 import React from "react";
+import {SpecData} from "../../data/spec";
 
 const columns = [
     {
@@ -12,41 +13,6 @@ const columns = [
         title: '内容',
         dataIndex: 'value',
         key: 'value',
-    },
-];
-const data = [
-    {
-        key: '1',
-        name: 'CPU',
-        value: 'Intel Core i7-7700K',
-    }, {
-        key: '2',
-        name: 'RAM',
-        value: 'Team DDR4 3000MHz 32GB',
-    }, {
-        key: '3',
-        name: 'SSD',
-        value: 'SDSSDA-240G-J26',
-    }, {
-        key: '4',
-        name: 'PU',
-        value: 'KRPW-GK750W',
-    }, {
-        key: '5',
-        name: 'CF',
-        value: '虎徹 Mark II',
-    }, {
-        key: '6',
-        name: 'MB',
-        value: 'PRIME H270-PRO',
-    }, {
-        key: '7',
-        name: 'CC',
-        value: 'Thermaltake Versa H26 Black',
-    }, {
-        key: '8',
-        name: 'OS',
-        value: 'Linux',
     },
 ];
 
@@ -69,7 +35,7 @@ function Spec() {
         >
             <Table
                 columns={columns}
-                dataSource={data}
+                dataSource={SpecData}
                 size="small"
                 bordered={true}
                 pagination={false}
