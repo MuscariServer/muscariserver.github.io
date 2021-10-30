f2bead84bed538211b94d94b16bd1ef4e9199175
eight_y_88 <jadec.eight@gmail.com>
2021-10-30 15:48:18 +0900

    Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-
+      <HashRouter>
+          <div className="Container">
+          </div>
+      </HashRouter>
     </div>
   );
 }
