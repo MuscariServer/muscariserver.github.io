b29685628674364b5d110378989d9806a793bac0
eight_y_88 <jadec.eight@gmail.com>
2021-10-30 17:02:25 +0900

    TOP

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,30 @@ import Sidebar from "./sidebar";
 import 'styles/calendar.css'
 import 'styles/tabs.css'
 
+import TopPage from './pages/top'
+import Links from './pages/links'
+import Donate from './pages/donate'
+import Recruit from './pages/recruit'
+import Events from './pages/events'
+import Info from './pages/info'
+import JoinPage from './pages/join'
+
 function App() {
   return (
     <div className="App">
       <HashRouter>
           <Sidebar />
           <div className="Container">
-
+              <Switch>
+                  <Route path="/" exact component={TopPage} />
+                  <Route path="/join/:page" component={JoinPage} />
+                  <Route path="/join" component={JoinPage} />
+                  <Route path="/events" component={Events} />
+                  <Route path="/donate" component={Donate} />
+                  <Route path="/info" component={Info} />
+                  <Route path="/recruit" component={Recruit} />
+                  <Route path="/links" component={Links} />
+              </Switch>
           </div>
       </HashRouter>
     </div>
