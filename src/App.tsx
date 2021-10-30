8f1cd5ba1872ca5bf9095016e6750a3fdebf195d
eight_y_88 <jadec.eight@gmail.com>
2021-10-30 16:39:54 +0900

    CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Sidebar from "./sidebar";
 
+import 'styles/calendar.css'
+import 'styles/tabs.css'
+
 function App() {
   return (
     <div className="App">
