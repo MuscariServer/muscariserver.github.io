a244b7867bb97f9e238b2eaf98452d61ed6a7c94
eight_y_88 <jadec.eight@gmail.com>
2022-01-21 19:53:10 +0900

    Update current.tsx

--- src/data/current.tsx
+++ src/data/current.tsx
@@ -1,8 +1,8 @@
 export const Modpack_data = {
     "name": "MCS 1",
-    "version": "1.0.2",
+    "version": "1.0.4",
     "author": "Muscari DevTeam",
     "description": "The Muscari Community Server ModPack.",
     "url": "https://github.com/MuscariServer/mcs1",
-    "zip": "https://github.com/MuscariServer/mcs1/releases/download/v1.0.2/MuscariPack-v1.0.2.zip"
+    "zip": "https://github.com/MuscariServer/mcs1/releases/download/v1.0.4/MuscariPack-v1.0.4.zip"
 };
